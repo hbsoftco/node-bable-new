b7b631a6629a99f86beb8cd49fea92546779f068
hbsoftco <hossein.bajan@gmail.com>
2020-02-20 15:38:18 +0330

    Update

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    development: {
+        username: 'root',
+        password: '1',
+        database: 'node_babel',
+        host: process.env.DB_HOST,
+        dialect: 'mariadb',
+        dialectOptions: {
+            timezone: 'Etc/GMT0',
+        },
+    },
+    test: {
+        username: process.env.CI_DB_USERNAME,
+        password: process.env.CI_DB_PASSWORD,
+        database: process.env.CI_DB_NAME,
+        host: process.env.CI_DB_HOSTNAME,
+        dialect: process.env.DB_CONNECTION
+    },
+    production: {
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOSTNAME,
+        dialect: process.env.DB_CONNECTION
+    }
+};
